895459e6b19cdd6847176fe13229bcfc86aa9f0c
Erick Braga <erick.braga@laqus.com.br>
2024-06-06 12:32:00 -0300

    Comments

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -1,5 +1,5 @@
 return {
-	{
+  {
     -- útil para marcações e navegação
     -- Modo de uso:
     --
@@ -13,10 +13,35 @@ return {
     -- m}               -> Move para o próximo bookmark do mesmo tipo
     -- m{               -> Move para o bookmark anterior do mesmo tipo
     -- dm=              -> deleta o bookmark da linha que está o cursor
-		'chentoast/marks.nvim',
-		event = "VeryLazy",
-		config = function()
-			require('marks').setup({})
-		end
-	},
+    'chentoast/marks.nvim',
+    event = "VeryLazy",
+    config = function()
+      require('marks').setup({})
+    end
+  },
+  {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup({
+        toggler = {
+          line = 'gcc',
+          block = 'gbc',
+        },
+        opleader = {
+          line = 'gc',
+          block = 'gb',
+        },
+        mappings = {
+          basic = true,
+          extra = true,
+        },
+      })
+
+      -- Mapeamento no modo normal para 'Ctrl + k, Ctrl + c'
+      vim.api.nvim_set_keymap('n', '<C-\\>', '<Esc>gcc', { noremap = false })
+      vim.api.nvim_set_keymap('i', '<C-\\>', '<Esc>gcc', { noremap = false })
+      -- Mapeamento no modo visual para 'Ctrl + k, Ctrl + c'
+      vim.api.nvim_set_keymap('v', '<C-k><C-c>', 'gc', { noremap = false, silent = true })
+    end
+  }
 }
