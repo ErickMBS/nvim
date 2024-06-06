c05b4cae2b6793eea46ff810e430e0a94c1e31b7
ErickMBS <erickmth.braga@gmail.com>
2024-06-06 00:39:43 -0300

    Atalho Esc para sair do terminal integrado

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,10 +1,25 @@
 return {
+  -- Terminal Integrado
   "akinsho/toggleterm.nvim",
   config = function()
     require("toggleterm").setup({
-      size = 8,
-      direction = 'horizontal'
+      size = 20,
+      hide_numbers = true,
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      insert_mappings = true,
+      persist_size = true,
+      direction = 'horizontal', -- pode ser 'vertical' ou 'float'
+      close_on_exit = true,
+      shell = vim.o.shell,
+      -- Esta função é chamada quando o terminal é aberto
+      on_open = function(term)
+        vim.api.nvim_buf_set_keymap(term.bufnr, 't', '<C-\\><C-n>', [[<C-\><C-n>]], { noremap = true, silent = true })
+      end,
     })
     vim.keymap.set('n', '<leader>te', ':ToggleTerm size=8<CR>', {})
+    vim.api.nvim_set_keymap('t', '<Esc>', [[<C-\><C-n>]], {noremap = true, silent = true})
   end
 }
